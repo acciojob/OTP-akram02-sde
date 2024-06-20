ab144759743fa8b30707ee93e6f940ff75412cab
Akram Ahmed Khan <68415682+akram02-sde@users.noreply.github.com>
2024-06-20 11:07:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+
+    const codes = document.querySelectorAll(".code");
+
+    codes.forEach((code, index) => {
+        code.addEventListener("input", (e) => {
+            const value = e.target.value;
+            if (value) {
+                if (index < codes.length - 1) {
+                    // Move focus to the next input
+                    codes[index + 1].focus();
+                }
+            }
+        });
+
+        code.addEventListener("keydown", (e) => {
+            if (e.key === "Backspace" && !e.target.value) {
+                if (index > 0) {
+                    // Move focus to the previous input
+                    codes[index - 1].focus();
+                }
+            }
+        });
+    });
