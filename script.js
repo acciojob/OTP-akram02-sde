37dabcdccf8f5bd7cdfc1ae24cb43fc04400b50f
Akram Ahmed Khan <68415682+akram02-sde@users.noreply.github.com>
2024-07-19 18:26:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-
-    const codes = document.querySelectorAll(".code");
-
-    codes.forEach((code, index) => {
-        code.addEventListener("input", (e) => {
-            const value = e.target.value;
-            if (value) {
-                if (index < codes.length - 1) {
-                    // Move focus to the next input
-                    codes[index + 1].focus();
-                }
-            }
-        });
-
-        code.addEventListener("keydown", (e) => {
-            if (e.key === "Backspace" && !e.target.value) {
-                if (index > 0) {
-                    // Move focus to the previous input
-                    codes[index - 1].focus();
-                }
-            }
-        });
-    });
+//your JS code here. If required.
+let inputElements = document.getElementsByClassName("code");
+console.log(inputElements.length)
+for(let i=0; i< inputElements.length; i++){
+	inputElements[i].addEventListener("keyup", (event)=>{
+		let currentElement = event.target ;
+		let code = event.key.charCodeAt(0);
+		console.log(event.key)
+		if(event.key === "Backspace"){
+			let prevElement = currentElement.previousElementSibling ;
+			if(prevElement){
+				prevElement.focus();
+			}
+			return ;
+		}
+		
+		
+		
+		if(code>=48 && code<=57){
+			let nextElement = currentElement.nextElementSibling ;
+			if(nextElement){
+				nextElement.focus() ;
+			}
+			
+		}else{
+			event.target.value="" ;
+		}
+	})
+	
+}
